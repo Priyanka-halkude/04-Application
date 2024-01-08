aa854b65c78f9739a9623fa48abd80f0b75dc932
Priyanka <hpriyanka21@gmail.com>
2024-01-08 13:39:58 +0530

    x and y values added

--- src/main/java/com/cjc/App.java
+++ src/main/java/com/cjc/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! priyanka" );
+      
+int x=10;
+int y=20;
+System.out.println(x+ ' '+y);
+
+
     }
 }
